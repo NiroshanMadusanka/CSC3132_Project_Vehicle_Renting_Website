9eb6ed796c4672ab46ea921ce1bcd9299d9b6458
NiroshanMadusanka <niroshanmadu531@gmail.com>
2024-11-30 10:24:11 +0530

    Home Page Of RENT & RIDE

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const searchButton = document.querySelector(".search-bar button");
+  searchButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    alert("Search functionality is not yet implemented.");
+  });
+});
