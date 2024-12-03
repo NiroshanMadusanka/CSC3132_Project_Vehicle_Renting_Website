1d719d536ab1e5cabe3c9b06058c6fd510631fa8
D. M. Niroshan Madusanka Dissanayaka <120000451+NiroshanMadusanka@users.noreply.github.com>
2024-12-03 05:50:30 +0000

    git ignore created

--- connectDB.php
+++ connectDB.php
@@ -1,8 +1,14 @@
 <?php
+/*
+$servername = getenv('DB_HOST');
+$username = getenv('DB_USER'); 
+$password = getenv('DB_PASS');
+$dbname = getenv('DB_NAME');
+*/
 $servername = "127.0.0.1:3306";
-$username = "root"; 
-$password = "mariadb"; 
-$dbname = "vehicle_renting"; 
+$username = "root";
+$password = "mariadb";
+$dbname = "vehicle_renting";
 
 
 $conn = new mysqli($servername, $username, $password, $dbname);
