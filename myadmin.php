73bfd79390d1b85f481834cfbd6b2776bc2606f9
D. M. Niroshan Madusanka Dissanayaka <120000451+NiroshanMadusanka@users.noreply.github.com>
2024-12-03 04:57:07 +0000

    created admin dashborad.Not added admin functions yet

---
+++ myadmin.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>RENT & RIDE</title>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+    />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="style.css" />
+    <link
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+      rel="stylesheet"
+    />
+    <style>
+      body {
+        background: url("img/background_Home.jpg") no-repeat center center fixed;
+        background-size: cover;
+        padding-top: 70px;
+      }
+    </style>
+  </head>
+
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <a class="navbar-brand d-flex align-items-center" href="index.html">
+          <img
+            src="img/LogoNew.jpg"
+            alt="Rent & Ride Logo"
+            style="height: 40px; margin-right: 10px"
+          />
+          Rent & Ride
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="index.html">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="vehicles.html">Vehicles</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="about.html">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="contact.html">Contact</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link btn btn-primary text-white" href="login.html"
+                >Login</a
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+<footer class="bg-dark text-white text-center py-4">
+      <div>
+        <a
+          href="https://www.facebook.com/"
+          target="_blank"
+          class="text-white mx-2"
+        >
+          <i class="fab fa-facebook fa-2x"></i>
+        </a>
+        <a
+          href="https://www.twitter.com/"
+          target="_blank"
+          class="text-white mx-2"
+        >
+          <i class="fab fa-twitter fa-2x"></i>
+        </a>
+        <a
+          href="https://www.instagram.com/"
+          target="_blank"
+          class="text-white mx-2"
+        >
+          <i class="fab fa-instagram fa-2x"></i>
+        </a>
+        <a
+          href="https://www.linkedin.com/"
+          target="_blank"
+          class="text-white mx-2"
+        >
+          <i class="fab fa-linkedin fa-2x"></i>
+        </a>
+      </div>
+      <p>&copy; 2024 Rent & Ride. All rights reserved.</p>
+    </footer>
+
+    <script src="script.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
